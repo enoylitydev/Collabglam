02517ea0e78673d33045e18139e527bae8663dc0
Devansh <devanshd78@gmail.com>
2025-11-27 12:47:54 +0530

    hggh

--- src/app/brand/(protected)/browse-influencer/page.tsx
+++ src/app/brand/(protected)/browse-influencer/page.tsx
@@ -1,10 +1,24 @@
-"use client";
+// "use client";
 
-import React from 'react';
+// import React from 'react';
+// import ModashDashboard from './ModashDashboard';
+
+// function BrowseInf() {
+//   return <ModashDashboard />;
+// }
+
+// export default BrowseInf;
+
+// app/brand/(protected)/browse-influencer/page.tsx
+
+"use client";
+import { Suspense } from 'react';
 import ModashDashboard from './ModashDashboard';
 
-function BrowseInf() {
-  return <ModashDashboard />;
+export default function BrowseInfluencerPage() {
+  return (
+    <Suspense fallback={null}>
+      <ModashDashboard />
+    </Suspense>
+  );
 }
-
-export default BrowseInf;
