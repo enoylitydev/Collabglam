98a2caec4ceddbee6fa7527c6ceaedb1a814710a
Devansh <devanshd78@gmail.com>
2026-03-09 11:03:44 +0530

    updates

--- src/app/admin/(protected)/brands/shortlisted-inf/page.tsx
+++ src/app/admin/(protected)/brands/shortlisted-inf/page.tsx
@@ -15,7 +15,7 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { FloatingLabelInput } from "@/components/common/FloatingLabelInput";
-import AdminMilestoneHistoryCard from "@/components/common/AdminMilestoneHistoryCard";
+import AdminMilestoneHistoryCard from "@/components/common/AdminMIlestoneHistoryCard";
 import {
   HiChevronLeft,
   HiChevronRight,
